5745dacb2dc41d472eeaae670d8479cf2ababde5
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-02-03 17:14:12 +0300

    Fixed cards

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,25 +1,21 @@
 function Brands() {
-    return (
-        <
-            div className ="flex flex-row justify-center">
-            <div className="bg-white shadow-xl shadow-[#9e1b1d] rounded-full w-60 h-60 flex justify-center items-center mx-5">
-                hey
-            </div>
-            <div className="bg-white shadow-xl shadow-[#9e1b1d] rounded-full w-60 h-60 flex justify-center items-center mx-5">
-                hey
-            </div>
-            <div className="bg-white shadow-xl shadow-[#9e1b1d] rounded-full w-60 h-60 flex justify-center items-center mx-5">
-                hey
-            </div>
-            <div className="bg-white shadow-xl shadow-[#9e1b1d] rounded-full w-60 h-60 flex justify-center items-center mx-5">
-            hey
-        </div>
+  return (
+    <div className="flex flex-row justify-center py-10">
+      <div className="bg-white shadow-2xl shadow-[#9e1b1d] rounded-full w-60 h-60 flex justify-center items-center mx-5">
+        hey
+      </div>
+      <div className="bg-white shadow-2xl shadow-[#9e1b1d] rounded-full w-60 h-60 flex justify-center items-center mx-5">
+        hey
+      </div>
+      <div className="bg-white shadow-2xl shadow-[#9e1b1d] rounded-full w-60 h-60 flex justify-center items-center mx-5">
+        hey
+      </div>
 
-
-
-
-        </div>
-    );
+      <div className="bg-white shadow-2xl shadow-[#9e1b1d] rounded-full w-60 h-60 flex justify-center items-center mx-5 ">
+        hey
+      </div>
+    </div>
+  );
 }
 
 export default Brands;
