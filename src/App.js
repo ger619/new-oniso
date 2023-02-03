280833ca28839525ec96c672e41a4cbf792051e4
ger619 <draker74@gmail.com>
2023-02-03 12:42:47 +0300

    Added nav image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import NavBar from "./components/NavBar";
+import Brands from "./components/Brands";
 function App() {
     return (
         <
         div className ="text-center">
-            ubnm\
+
             <NavBar/>
+
+            <Brands/>
 
 
         </div>
